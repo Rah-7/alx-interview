415b8f99db90c1a3c55b190e5ecb8b7174cef387
Rah-7 <rahigirmam16@gmail.com>
2023-11-23 21:43:24 +0300

    change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,15 @@
 def makeChange(coins, total):
-    if total < 1:
+    if total < 0:
+        return -1
+    if total == 0:
         return 0
+
     dp =  * (total + 1)
     dp = 0
+
     for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
                 dp = min(dp, dp + 1)
-    
+
     return dp if dp != float('inf') else -1
