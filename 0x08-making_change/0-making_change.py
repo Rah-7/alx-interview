027dc50ae014d19d43531224b0ec984f382d453d
Rah-7 <rahigirmam16@gmail.com>
2023-11-23 21:58:49 +0300

    new

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,28 @@
+#!/usr/bin/python3
+"""Change making module.
+"""
+
+
 def makeChange(coins, total):
-    if total < 0:
-        return -1
-    if total == 0:
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
         return 0
-
-    dp =  * (total + 1)
-    dp = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp = min(dp, dp + 1)
-
-    return dp if dp != float('inf') else -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins >= 0:
+            rem -= sorted_coins
+            coins_count += 1
+        else:
+            coin_idx += 1
+    if rem > 0:
+        return -1
+    else:
+        return coins_count
